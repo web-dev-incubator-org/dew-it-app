b4b2320abe2f3ae60b0a99bfcd8341f6c27b87c0
abstractdev <fadcny@gmail.com>
2022-05-25 18:39:07 -0400

    getDews initial

---
+++ functions/getDews.js
@@ -0,0 +1,22 @@
+//Import the functions you need. Read the firebase docs or ask if you're not sure which ones you need.
+//You will generally always need collection
+import {
+  collection,
+  getDocs,
+  query,
+  orderBy,
+} from "https://www.gstatic.com/firebasejs/9.0.2/firebase-firestore.js";
+//import the database instance
+import { db } from "../firebaseconfig.js";
+
+//we are exporting an async function to app.js. The default part is optional but I see it often and it seems to be common practice. It allows you to name the import anything you want AND forces you to omit the {} when importing. This must be async because we must wait for firebase to send us the data before running the rest of the function.
+export default async function getDews() {
+  //this is just a reference to the collection stored as a const
+  const dewsRef = collection(db, "dews");
+  //this is the query function that gets the dews and orders them by Timestamp, oldest to newest
+  const querySnapshot = await getDocs(query(dewsRef, orderBy("createdAt")));
+  //console.log each dew
+  querySnapshot.forEach((doc) => {
+    console.log(doc.data());
+  });
+}
