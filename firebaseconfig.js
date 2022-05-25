1cc3dc5222eaa82d96f51168b29f05f042105709
abstractdev <fadcny@gmail.com>
2022-05-25 18:38:30 -0400

    fix import

--- firebaseconfig.js
+++ firebaseconfig.js
@@ -1,20 +1,16 @@
-import { getFirestore } from "firebase/firestore";
-import { initializeApp } from "firebase/app";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.2/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.0.2/firebase-firestore.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAiydtot56YvYPFcHHPUjWnStkpuObavs0",
-    authDomain: "dew-it-app.firebaseapp.com",
-    databaseURL: "https://dew-it-app-default-rtdb.firebaseio.com",
-    projectId: "dew-it-app",
-    storageBucket: "dew-it-app.appspot.com",
-    messagingSenderId: "523712062062",
-    appId: "1:523712062062:web:7869678afc18406981fc7f",
-    measurementId: "G-3YZM7MVTSJ"
-  };
-  
+  apiKey: "AIzaSyAiydtot56YvYPFcHHPUjWnStkpuObavs0",
+  authDomain: "dew-it-app.firebaseapp.com",
+  databaseURL: "https://dew-it-app-default-rtdb.firebaseio.com",
+  projectId: "dew-it-app",
+  storageBucket: "dew-it-app.appspot.com",
+  messagingSenderId: "523712062062",
+  appId: "1:523712062062:web:7869678afc18406981fc7f",
+  measurementId: "G-3YZM7MVTSJ",
+};
+
 const firebaseApp = initializeApp(firebaseConfig);
 export const db = getFirestore(firebaseApp);
-
-// Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
